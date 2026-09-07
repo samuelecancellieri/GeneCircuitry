2355b5bc586f9e51b639b046af46f0f81816994b
samuelecancellieri <samuelec113@gmail.com>
2026-09-07 14:07:28 +0200

    feat: Add boolean configuration parameters to config types test

--- tests/test_config.py
+++ tests/test_config.py
@@ -145,6 +145,7 @@ def test_config_types():
         assert isinstance(cfg[param], str), f"{param} should be string"
 
     # Boolean values
+    bool_params = ["VERBOSE", "LOW_MEMORY", "FORCE_DIM_REDUCTION", "SAVE_PDF"]
     bool_params = [
         "VERBOSE",
         "LOW_MEMORY",
